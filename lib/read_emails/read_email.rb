d89378a6a497bcd82fa4b45e2bf0492d0fc65ca5
claudiug <claudiu.garba@gmail.com>
2015-07-12 17:34:21 +0200

    add basic library to read email

---
+++ lib/read_emails/read_email.rb
@@ -0,0 +1,14 @@
+class ReadEmail
+
+  def initialize(report)
+    @email = report.generated_email
+  end
+
+  #TODO
+
+  #get one email
+  #get all emails
+  #get information
+  #save them??
+  #use google gmail acount?
+end
